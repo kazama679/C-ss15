815eaa49bbf55928844fbc62a488afb52ee8f474
kazama679 <147390572+kazama679@users.noreply.github.com>
2023-12-01 11:20:13 +0700

    Add files via upload

---
+++ thuc hanh2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+struct DiemThi {
+    int maSV;
+    float diemToan;
+    float diemVan;
+};
+int main() {
+    struct DiemThi dsDiemThi[5];
+    // Nh?p thông tin ði?m thi cho 5 sinh viên
+    for (int i = 0; i < 5; i++) {
+        printf("Nhap ma sinh vien: ");
+        scanf("%d", &dsDiemThi[i].maSV);
+        printf("Nhap diem toan: ");
+        scanf("%f", &dsDiemThi[i].diemToan);
+        printf("Nhap diem van: ");
+        scanf("%f", &dsDiemThi[i].diemVan);
+    }
+    printf("\nDanh sach diem thi:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Ma sinh vien: %d, Diem toan: %.2f, Diem van: %.2f\n",
+               dsDiemThi[i].maSV, dsDiemThi[i].diemToan, dsDiemThi[i].diemVan);
+    }
+    return 0;
+}
